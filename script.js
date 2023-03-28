ffebafd7db14b6c172ba5f701e6ed9484dccdbdd
Miles Kerr <m.kerr.professional@gmail.com>
2023-03-28 13:12:36 -0500

    new landing page new bootstrap

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-function calculateAverage() {
-    // Get all the radio buttons with the name "shotshape"
-    const buttons = document.getElementsByName("shotshape");
 
-    // Calculate the total of the selected values
-    let total = 0;
-    let count = 0;
-    for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i].checked) {
-            total += parseInt(buttons[i].value);
-            count++;
-        }
-    }
-
-    // Calculate the average and display it
-    const average = total / count;
-    document.getElementById("average").value = average;
-}
