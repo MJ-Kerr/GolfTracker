32550b5a11c4220dbeeb7f3d60c9f4c2176569b0
Miles Kerr <m.kerr.professional@gmail.com>
2023-03-30 11:24:55 -0500

    updated code after stuff was gone

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
-let map;
+const golfCourse = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '54ef2a37a8msh7fb788601a760ccp117419jsn30a11b1f92eb',
+        'X-RapidAPI-Host': 'golf-course-finder.p.rapidapi.com'
+    }
+};
 
-async function initMap() {
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps");
+fetch('https://golf-course-finder.p.rapidapi.com/courses?radius=10&lat=36.56910381018662&lng=-121.95035631683683', golfCourse)
+    .then(response => response.json())
+    .then(response => console.log(response))
+.catch(err => console.error(err));
 
-    map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-}
+// fetch('https://golf-course-finder.p.rapidapi.com/courses?radius=10&lat=36.56910381018662&lng=-121.95035631683683', options)
+// .then(response => response.json())
+// .then(response => { 
+    // const golfCourses = response.results;
+	// const tbody = $('#golf-courses');
+	// golfCourses.forEach(golfCourse => {
+		// const row = `
+			// <tr>
+				// <td>${golfCourse.name}</td>
+				// <td>${golfCourse.address}</td>
+				// <td>${golfCourse.city}</td>
+				// <td>${golfCourse.state}</td>
+			// </tr>
+		// `;
+		// tbody.append(row);
+	// });
+// })
 
-initMap();
+
+// .catch(err => console.error(err));
+// function printArrayAverage(arr) {
+//     if (arr.length == 0) {
+//         console.log("Empty arr, no average val");
+//         return;
+//     }
+//     var sum = arr[0];
+//     for (var idx = 1; idx < arr.length; idx++) {
+//         sum += arr[idx];
+//     }
+//     return sum / arr.length;
+// }
+
+// document.getElementById("driver-return").innerText = printArrayAverage();
+
+
+// let numArray = [];
+
+// const input = document.getElementById('num-input-driver');
+// const btn = document.getElementById('submit-btn-driver');
+
+
+// btn.addEventListener('click', function () {
+//     let num = parseInt(input.value);
+//     numArray.push(num);
+
+// printArrayAverage(numArray);
+// });
