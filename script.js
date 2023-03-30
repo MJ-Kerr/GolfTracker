4f9d87ed489d6effeda537a1de225286be2fe1c2
Miles Kerr <m.kerr.professional@gmail.com>
2023-03-30 15:46:01 -0500

    EVERYTHING IS FUNCTIONAL

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ const golfCourse = {
 
 fetch('https://golf-course-finder.p.rapidapi.com/courses?radius=10&lat=36.56910381018662&lng=-121.95035631683683', golfCourse)
     .then(response => response.json())
-    .then(response =>{ console.log(response)
-    const golfCourses = response.courses.slice(0, 3);
-	const tbody = $('#golf-courses');
-	golfCourses.forEach(golfCourse => {
-		const row = `
+    .then(response => {
+        console.log(response)
+        const golfCourses = response.courses.slice(0, 3);
+        const tbody = $('#golf-courses');
+        golfCourses.forEach(golfCourse => {
+            const row = `
 			<tr>
 				<td>${golfCourse.name}</td>
 				<td>${golfCourse.zip_code}</td>
 			</tr>
 		`;
-		tbody.append(row);
-	});
-})
-.catch(err => console.error(err));
-
+            tbody.append(row);
+        });
+    })
+    .catch(err => console.error(err));
 
 
 function printArrayAverage(arr) {
@@ -34,21 +34,36 @@ function printArrayAverage(arr) {
     for (var idx = 1; idx < arr.length; idx++) {
         sum += arr[idx];
     }
-    return sum / arr.length;
+    return Math.floor(sum / arr.length);
 }
 
-// document.getElementById("driver-return").innerText = printArrayAverage();
+let driverArray = [];
+let ironArray = [];
+let wedgeArray = [];
 
+const driverInput = document.getElementById('driver-input');
+const driverBtn = document.getElementById('submit-btn-driver');
+driverBtn.addEventListener('click', function () {
+    let num = parseInt(driverInput.value);
+    driverArray.push(num);
+    let average = printArrayAverage(driverArray);
+    document.getElementById("driver-return").innerText = average;
+});
 
-let numArray = [];
+const ironInput = document.getElementById('iron-input');
+const ironBtn = document.getElementById('submit-btn-iron');
+ironBtn.addEventListener('click', function () {
+    let num = parseInt(ironInput.value);
+    ironArray.push(num);
+    let average = printArrayAverage(ironArray);
+    document.getElementById("iron-return").innerText = average;
+});
 
-const input = document.getElementById('driver-input');
-const btn = document.getElementById('submit-btn-driver');
-
-
-btn.addEventListener('click', function () {
-    let num = parseInt(input.value);
-    numArray.push(num);
-
-printArrayAverage(numArray);
+const wedgeInput = document.getElementById('wedge-input');
+const wedgeBtn = document.getElementById('submit-btn-wedge');
+wedgeBtn.addEventListener('click', function () {
+    let num = parseInt(wedgeInput.value);
+    wedgeArray.push(num);
+    let average = printArrayAverage(wedgeArray);
+    document.getElementById("wedge-return").innerText = average;
 });
